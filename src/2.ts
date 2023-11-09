2d2e07516c594e7513077ecd5c7c79a331dda4fb
RimmaO <rimma_o@ukr.net>
2023-11-09 23:15:02 +0200

    done 1-3 to check 4 - ask

--- src/2.ts
+++ src/2.ts
@@ -14,17 +14,33 @@
 
 */
 
-class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+// class Employee {
+//   // Заповніть модифікатори доступу
+//   name: string;
+//   department: string;
+//   salary: number;
 
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
+//   constructor(name: string, department: string, salary: number) {
+//     this.name = name;
+//     this.department = department;
+//     this.salary = salary;
+//   }
+
+//   getEmployeeDetails() {
+//     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+//   }
+// }
+
+// class Manager extends Employee {
+//   // Реалізуйте конструктор та збільшіть salary на 10000
+// }
+
+class Employee {
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {}
 
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
@@ -32,8 +48,8 @@ class Employee {
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(salary: number) {
+    super("name", "department", salary + 10000);
+  }
 }
-
-
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,42 @@
+// const key = new Key();
+
+// const house = new MyHouse(key);
+// const person = new Person(key);
+
+// house.openDoor(person.getKey());
+
+// house.comeIn(person);
+
+class Key {
+  constructor(private signature: number) {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean;
+  key: Key;
+
+  abstract openDoor(key: Key);
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {}
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +45,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
